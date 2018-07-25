aa5729b1d58b83dd4dcf04a0593407a2a1f09c10
Tate Moskal <tate.moskal@gmail.com>
2018-07-25 09:29:52 -0500

    Adding a game to it

--- simonsays.py
+++ simonsays.py
@@ -2,23 +2,40 @@ import LEDRGB as LED
 import RPi.GPIO as GPIO
 import time
 import random
+from getpass import getpass
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 buzz_pin = 32
 
-colors = ['R','G','B','Y', 'C','P','O','D']
-frequencies = [220,660,880,440,330,550,770,110]
+colors = ['R','G','B','Y']
+frequencies = [220,660,880,440]
 colorlist = []
 soundlist = []
+
 R_pin = 11
 G_pin = 12
 B_pin = 13
 GPIO.setup(buzz_pin,GPIO.OUT)
 Buzz = GPIO.PWM(buzz_pin,1000)
 LED.setup(R_pin,G_pin,B_pin)
+def validate_guess(colorstring, guess):
+	colorstring.strip()
+	guess.strip()
+	if colorstring == guess:
+		print "Correct!"
+	else:
+		print "Nope!"
+		print "The answer was: " + colorstring
+		print "You answered:   " + guess
+		LED.destroy()
+		exit()
+	
+	
+
 def simonsays():
+
 	while True:
 		n = random.randint(0,3)
 		colorlist.append(colors[n])
@@ -35,6 +52,12 @@ def simonsays():
 			time.sleep(.5)
 			p = p - 1
 			x = x + 1
+		guess = getpass("Guess the color sequence: ")
+		colorstring = "".join(colorlist)
+		validate_guess(colorstring, guess.upper())
+			
+		
+		
 			
 if __name__ == '__main__':
 	try:
